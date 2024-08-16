1514f90b15323b92a79a8f7aa5cf1f3d74231077
hafizasad419 <hafizasad419@gmail.com>
2024-08-16 19:51:08 +0500

    make slider responsive

--- src/components/Home/SliderComponent/SliderComponent.jsx
+++ src/components/Home/SliderComponent/SliderComponent.jsx
@@ -35,7 +35,7 @@ const SliderComponent = () => {
   ];
 
   return (
-    <div className="z-[1] slider-container w-10/12 md:w-[100%] mx-auto mb-16">
+    <div className="z-[1] slider-container w-[100%] mx-auto mb-16">
       <Slider {...settings}>
         {slides.map((slide, index) => (
           <div key={index}>
